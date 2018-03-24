8efcb49e4686489e556c3dd1b5f849604ce3b03c
areph <40balmung@gmail.com>
2018-03-24 13:58:24 +0900

    fix.

--- camera.js
+++ camera.js
@@ -1,95 +1,29 @@
-let width = 320    // We will scale the photo width to this
-let height = 0     // This will be computed based on the input stream
+//video element
+var video = document.getElementById('camera');
 
-let streaming = false
+//video element（プレビュー画面）をクリックして撮影
+video.addEventListener("click", function() {
+  take_picture()
+});
 
-let video = null
-let canvas = null
-let photo = null
-let startbutton = null
-let constrains = { video: true, audio: false }
+//テキストクリック撮影（撮影トリガー 1）
+$("#rec").click(function() {
+  take_picture()
+});
 
-/**
- * ユーザーのデバイスによるカメラ表示を開始し、
- * 各ボタンの挙動を設定する
- *
- */
-function startup() {
-  video = document.getElementById('video')
-  canvas = document.getElementById('canvas')
-  photo = document.getElementById('photo')
-  startbutton = document.getElementById('startbutton')
+//撮影関数
+function take_picture() {
+  //videoのstreamをcanvasに書き出す方法
+  var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+//videoの縦幅横幅を取得
+    var w = video.offsetWidth;
+    var h = video.offsetHeight;
+    canvas.setAttribute("width", w);
+    canvas.setAttribute("height", h);
+    ctx.drawImage(video, 0, 0, w, h);
 
-  videoStart()
-
-  video.addEventListener('canplay', function(ev){
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width)
-
-      video.setAttribute('width', width)
-      video.setAttribute('height', height)
-      canvas.setAttribute('width', width)
-      canvas.setAttribute('height', height)
-      streaming = true
-    }
-  }, false)
-
-  // 「take photo」ボタンをとる挙動を定義
-  startbutton.addEventListener('click', function(ev){
-    takepicture()
-    ev.preventDefault()
-  }, false);
-
-  clearphoto()
+//canvasを更にimgに書き出す方法
+    var img = document.getElementById('img');
+    img.src = canvas.toDataURL('image/png');
 }
-
-/**
- * カメラ操作を開始する
- */
-function videoStart() {
-  streaming = false
-  navigator.mediaDevices.getUserMedia(constrains)
-  .then(function(stream) {
-      video.srcObject = stream
-      video.play()
-  })
-  .catch(function(err) {
-      console.log("An error occured! " + err)
-  })
-}
-
-/**
- * canvasの写真領域を初期化する
- */
-function clearphoto() {
-  let context = canvas.getContext('2d')
-  context.fillStyle = "#AAA"
-  context.fillRect(0, 0, canvas.width, canvas.height)
-}
-
-/**
- * カメラに表示されている現在の状況を撮影する
- */
-function takepicture() {
-  let context = canvas.getContext('2d')
-  if (width && height) {
-    canvas.width = width
-    canvas.height = height
-    context.drawImage(video, 0, 0, width, height)
-    send()
-  } else {
-    clearphoto()
-  }
-}
-
-function send() {
-    data = canvas.toDataURL('image/png').replace(/^.*,/, '')
-    $.ajax('/read.php',{
-        method: 'POST',
-        data: {image: data}
-    }).then(res => {
-        $('#readStr').val(res)
-    })
-}
-
-startup()
